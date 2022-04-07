0561e8785080fb6ae1dbf80665b9d919d3dacef3
Pedro Reis <pedro@komuhn.co>
2022-04-07 17:47:21 +0100

    Make initial CSS styles for readability

--- index.php
+++ index.php
@@ -16,66 +16,95 @@
     <!--Favicon Safari-->
     <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/Favicon-16x16.svg">
 
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
 
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 
     <header class="main-header">
-        <h1>Komuhn</h1>
+        <h1 class="logo">Komuhn</h1>
     </header>
 
-    <section class="about">
-        <h2>We're making a new website*</h2>
+    <section class="intro">
+        <h1>We're making a new website*</h1>
         <p><em>*If you're looking for our old website you can find it here: <a href="https://komuhn.co/old/">https://komuhn.co/old/</a></em></p>
 
-        <h3>Open design</h3>
+        <h2>Open design</h2>
         <p>We don't know how to do this. We really don't</p>
 
-        <h4>You can you participate?</h4>
+        <h4>How can you participate?</h4>
 
     </section>
 
+    <hr>
+
     <section class="posts">
-        <h2>Posts</h2>
+        <h2 class="section-title">Posts</h2>
 
         <article id="post-2022-02-25">
             <header>
                 <h3>Outgrowing.</h3>
                 <time datetime="2022-02-25">2022-02-25</time>
             </header>
-            <p>Post excerpt</p>
+            <p>Donec eu dignissim nibh. Praesent hendrerit lobortis arcu at volutpat. Quisque eu mi viverra, luctus augue non, luctus odio. Praesent quis tristique nibh, et tempor urna.</p>
+            <a href="#">Read more -></a>
+        </article>
+
+        <article id="post-2022-02-26">
+            <header>
+                <h3>Another post</h3>
+                <time datetime="2022-02-25">2022-02-25</time>
+            </header>
+            <p>Donec eu dignissim nibh. Praesent hendrerit lobortis arcu at volutpat. Quisque eu mi viverra, luctus augue non, luctus odio. Praesent quis tristique nibh, et tempor urna.</p>
             <a href="#">Read more -></a>
         </article>
 
     </section>
 
+    <hr>
+
     <section class="log">
-        <h2>Log</h2>
+        <h2 class="section-title">Log</h2>
+
+        <article id="log-2022-04-07">
+            <header>
+                <h3>Make it easier to read</h3>
+                <time datetime="2022-04-07">2022-04-07</time>
+            </header>
+            <ul>
+                <li>Make initial CSS styles for readability</li>
+            </ul>
+        </article>
 
         <article id="log-2022-03-15">
-            <h3>Customizing for the soul</h3>
-            <time datetime="2022-03-15">2022-03-15</time>
+            <header>
+                <h3>Customizing for the soul</h3>
+                <time datetime="2022-03-15">2022-03-15</time>
+            </header>
             <ul>
-                <li>Add a favicon, one of those tiny little images decorating your tabs, so you can quickly identify your its content.</li>
+                <li>Add a favicon, one of those tiny little images decorating your browser tabs, so you can quickly identify your tabs content.</li>
             </ul>
         </article>
 
         <article id="log-2022-03-10">
-            <h3>Published this page 🎉</h3>
-            <time datetime="2022-03-10">2022-03-10</time>
+            <header>
+                <h3>Published this page 🎉</h3>
+                <time datetime="2022-03-10">2022-03-10</time>
+            </header>
             <ul>
                 <li>Publish online</li>
-                <li>Make a cute image for our own wordpress theme - komuhn-website-v2 (check <a href="https://github.com/teamkomuhn/komuhn-website-v2.git">GitHub repository</a>) <img src="<?php echo get_template_directory_uri(); ?>/screenshot.png" alt="komuhn-website-v2 wordpress theme" style="max-width:150px; display:block;"/></li>
+                <li>Make a cute image for our own wordpress theme - komuhn-website-v2 (check <a href="https://github.com/teamkomuhn/komuhn-website-v2.git">GitHub repository</a>) <img src="<?php echo get_template_directory_uri(); ?>/screenshot.png" alt="komuhn-website-v2 wordpress theme" /></li>
                 <li>Setup wordpress and new theme</li>
                 <li>Organize old directories in our server > <a href="https://komuhn.co/old/">https://komuhn.co/old/</a></li>
             </ul>
         </article>
 
         <article id="log-2022-02-25">
-            <h3>Built this page ⚡</h3>
-            <time datetime="2022-02-25">2022-02-25</time>
+            <header>
+                <h3>Built this page ⚡</h3>
+                <time datetime="2022-02-25">2022-02-25</time>
+            </header>
             <ul>
                 <li>Add Google analytics script</li>
                 <li>Add initial content to page</li>
@@ -87,8 +116,10 @@
 
     </section>
 
+    <hr>
+
     <footer>
-        <h2>About this website</h2>
+        <h2 class="section-title">About this website</h2>
         <ul>
             <li>Discussion on <a href="#">Tribe?</a></li>
             <li>Tasks on <a href="#">Trello</a></li>
