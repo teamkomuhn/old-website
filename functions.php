2db7f0d4edcc2e8d2f086af9ea6fb7c9b95d22c2
Pedro Reis <pedro@komuhn.co>
2022-04-14 14:02:25 +0100

    Add a sticky post to the top of the page to introduce the website process

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+// ADD SUPPORT FOR MENUS
+add_theme_support( 'menus' );
 
 // REMOVE EMOJI FROM STRINGS https://stackoverflow.com/a/65179618
 function removeEmoji( string $text ): string {
@@ -7,6 +9,24 @@ function removeEmoji( string $text ): string {
     return iconv('ISO-8859-15', 'UTF-8', $text);
 };
 
-// ADD SUPPORT FOR MENUS
-add_theme_support( 'menus' );
+// RETURN UNIQUE READABLE POST ID
+// function make_unique_id( $str, $date, $pre ) {
+//     $slug = removeEmoji( $str );
+//     $slug = sanitize_title( $slug  );
+//     $unique_id = $slug . '-' . $date;
+//     if ( $pre ) {
+//         $unique_id =  $pre . '-' . $unique_id;
+//     }
+//     return 'Foo';//$unique_id;
+// }
+
+function make_unique_id( string $str, string $date, $pre ): string { // $pre can be a string or a boolean: false
+    $slug = removeEmoji( $str );
+    $slug = sanitize_title( $slug );
+    $unique_id = $slug . '-' . $date;
+    if ( $pre ) {
+        $unique_id =  $pre . '-' . $unique_id;
+    }
+    return $unique_id;
+}
 ?>
--- index.php
+++ index.php
@@ -4,16 +4,41 @@
         <h1>We're making a new website*</h1>
         <p><em>*If you're looking for our old website you can find it here: <a href="https://komuhn.co/old/">https://komuhn.co/old/</a></em></p>
 
-        <h2>Open design</h2>
-        <p>We don't know how to do this. We really don't</p>
+        <?php // GET STICKY POST
+        $stickies = get_option( 'sticky_posts' );
+        // Make sure we have stickies to avoid unexpected output https://wordpress.stackexchange.com/questions/202896/query-only-sticky-posts
+        if ( $stickies ) :
+            $args = [
+                'post_type'           => 'post',
+                'post__in'            => $stickies,
+                'posts_per_page'      => 1,
+                'ignore_sticky_posts' => 1
+            ];
+            $ko_sticky = new WP_Query( $args );
+            if ( $ko_sticky -> have_posts() ) :
+                while ( $ko_sticky -> have_posts() ): $ko_sticky -> the_post();
+                //Make unique readable post ID
+                $post_id = make_unique_id( get_the_title(), get_the_date('Y-m-d'), false );
+        ?>
 
-        <h4>How can you participate?</h4>
+                <article id="<?php echo $post_id; ?>">
+                    <header>
+                        <h3><?php the_title(); ?></h3>
+                        <time datetime="<?php echo get_the_date('c'); ?>"><?php print get_the_date('F j, Y'); ?></time>
+                    </header>
+                    <p><?php the_excerpt(); ?></p>
+                    <a href="<?php the_permalink(); ?>">Read more -></a>
+                </article>
 
+                <?php endwhile; ?>
+            <?php endif; wp_reset_postdata(); ?>
+        <?php endif; ?>
     </section>
 
     <?php //GET POSTS
     $args = array(
         'post_type'         => array('post'),
+        'post__not_in'      => $stickies,
         'category_name'     => 'post',
         'post_status'       => array('publish'),
     	'posts_per_page'    => '10',
@@ -30,21 +55,20 @@
     <section class="posts">
         <h2 class="section-title">Posts</h2>
 
-        <?php while ( $ko_posts -> have_posts() ): $ko_posts -> the_post(); ?>
-            <?php //Make unique readable post ID
-            $slug = removeEmoji( get_the_title() );
-            $slug = sanitize_title( $slug  );
-            $post_id = $slug . '-' . get_the_date('Y-m-d');
-            ?>
+        <?php
+        while ( $ko_posts -> have_posts() ): $ko_posts -> the_post();
+        //Make unique readable post ID
+        $post_id = make_unique_id( get_the_title(), get_the_date('Y-m-d'), false );
+        ?>
 
-            <article id="<?php echo $post_id; ?>">
-                <header>
-                    <h3><?php the_title(); ?></h3>
-                    <time datetime="<?php echo get_the_date('c'); ?>"><?php print get_the_date('F j, Y'); ?></time>
-                </header>
-                <p><?php the_excerpt(); ?></p>
-                <a href="<?php the_permalink(); ?>">Read more -></a>
-            </article>
+        <article id="<?php echo $post_id; ?>">
+            <header>
+                <h3><?php the_title(); ?></h3>
+                <time datetime="<?php echo get_the_date('c'); ?>"><?php print get_the_date('F j, Y'); ?></time>
+            </header>
+            <p><?php the_excerpt(); ?></p>
+            <a href="<?php the_permalink(); ?>">Read more -></a>
+        </article>
 
         <?php endwhile; ?>
     <?php endif; wp_reset_postdata(); ?>
@@ -70,20 +94,19 @@
     <section class="log">
         <h2 class="section-title">Log</h2>
 
-        <?php while ( $ko_logs -> have_posts() ): $ko_logs -> the_post(); ?>
-            <?php //Make unique readable post ID
-            $slug = removeEmoji( get_the_title() );
-            $slug = sanitize_title( $slug  );
-            $log_id = 'log-' . $slug . '-' . get_the_date('Y-m-d');
-            ?>
+        <?php
+        while ( $ko_logs -> have_posts() ): $ko_logs -> the_post();
+        //Make unique readable post ID
+        $log_id = make_unique_id( get_the_title(), get_the_date('Y-m-d'), 'log' );
+        ?>
 
-            <article id="<?php echo $log_id; ?>">
-                <header>
-                    <h3><?php the_title(); ?></h3>
-                    <time datetime="<?php echo get_the_date('c'); ?>"><?php print get_the_date('F j, Y'); ?></time>
-                </header>
-                <?php the_content(); ?>
-            </article>
+        <article id="<?php echo $log_id; ?>">
+            <header>
+                <h3><?php the_title(); ?></h3>
+                <time datetime="<?php echo get_the_date('c'); ?>"><?php print get_the_date('F j, Y'); ?></time>
+            </header>
+            <?php the_content(); ?>
+        </article>
 
         <?php endwhile; ?>
     <?php endif; wp_reset_postdata(); ?>
