b17325c5aa8ec5dd03df8f87351d4a8771603a00
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-04-07 15:57:36 +0100

    WIP desktop

--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// function enqueue() {
-// 	wp_enqueue_style('styles', 'wp-content/themes/komuhn.co/styles.css');
-// }
-
-// add_action('wp_enqueue_scripts', 'enqueue');
--- page-care.php
+++ page-care.php
@@ -7,49 +7,156 @@
 
         <link rel="stylesheet" href="/wp-content/themes/komuhn.co/styles.css">
 
+        <?php wp_head() ?>
+
         <title>Care</title>
     </head>
-
-    <!-- mark, ::marker, underline-->
     
     <body>
         <nav>
-            <h1><a href="/"><span id="ko">Ko</span>muhn</a></h1>
+            <a href="/">Komuhn</a>
         </nav>
 
         <main>
             <header>
-                <h2>Fluency<br />in <span id="care">care</span></h2>
+                <h1>Fluency<br />in <span id="care">care</span></h1>
 
                 <div>
-                    <h3>Thriving organazations through empathy</h3>
+                    <h2>Thriving organazations through empathy</h2>
 
-                    <p>
-                        <strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.
-                    </p>
+                    <p><strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.</p>
 
-                    <p>
-                        In this proposal, we are exploring ways towards more sustainable collaborations by understanding the value of beeing together.
-                    </p>
+                    <p>In this proposal, we are exploring ways towards more sustainable collaborations by understanding the value of beeing together.</p>
                 </div>
             </header>
 
-            <section id="first">
+            <aside>
+                <img src="...">
+
+                <p>Within many types of organizations — care — is often seen as something important, still, there's this uncertanty about its impact and how to put it into practice. How do we go about defining and manifesting it?</p>
+
+                <a class="button" href="#">Read the post</a>
+            </aside>
+
+            <section id="learnings">
+                <div class="side">
+                    <h2>Learnings →</h2>
+
+                    <p>We've been signaling patterns where we recongnize opportunities for building processes and turning practicies, that facilitate participation fulfillment, and involvement.</p>
+                </div>
+
                 <ul>
-                    <li><img src="..."/>Identify opportunities for raising capacity</li>
-                    <li><img src="..."/>Recognize power imbalences, understand its cause, and learn to propose distribution</li>
-                    <li><img src="..."/>Enable practices that create awereness of positive habits and behaviors, and help detect obstacles to progress</li>
-                    <li><img src="..."/>Facilitate activities that amplify strengths and consider everyone's needs</li>
-                    <li><img src="..."/>Apply confilict as a device for supporting diversity</li>
-                    <li><img src="..."/>Culture-in uncertain and complexity and tranform resistance into vulnerability and curiosity</li>
+                    <li><img src="..." />Identify opportunities for raising capacity</li>
+                    <li><img src="..." />Recognize power imbalences, understand its cause, and learn to propose distribution</li>
+                    <li><img src="..." />Enable practices that create awereness of positive habits and behaviors, and help detect obstacles to progress</li>
+                    <li><img src="..." />Facilitate activities that amplify strengths and consider everyone's needs</li>
+                    <li><img src="..." />Apply confilict as a device for supporting diversity</li>
+                    <li><img src="..." />Culture-in uncertain and complexity and tranform resistance into vulnerability and curiosity</li>
                 </ul>
             </section>
 
-            <!-- <section>
-                <p><strong>Fluency in care</strong> is our peoposal to anyone looking into fostering a collaborative culture that propagates from a deeper understanding of one another.</p>
+            <section id="why-are-we-together">
+                <h2>Why are we together?</h2>
 
-                <p>It's an invitation to explore new and sometimes unexpected approches towards learning about what we need, to be able to work well together.</p>
-            </section> -->
+                <p><strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.</p>
+
+                <div class="side">
+                    <h3>Themes and topics →</h3>
+            
+                    <p>We've been signaling patterns where we recongnize opportunities for building processes and turning practicies, that facilitate participation fulfillment, and involvement.</p>
+                </div>
+
+                <div>
+                    <article>
+                        <h4>Mapping needs</h4>
+
+                        <p>Who we are changes how we are. Make visible the needs of An exercise to make visible the needs</p>
+
+                        <p>outline everybody's needs and facilitate conversations</p>
+                    </article>
+
+                    <article>
+                        <h4>Beyond rules</h4>
+
+                        <p>What if we could organize and be without punishment and</p>
+                    </article>
+
+                    <article>
+                        <h4>Bending time</h4>
+
+                        <p>Description</p>
+                    </article>
+
+                    <article>
+                        <h4>Rythym and routines</h4>
+
+                        <p>Description</p>
+                    </article>
+
+                    <article>
+                        <h4>Bending time</h4>
+
+                        <p>Description</p>
+                    </article>
+
+                    <article>
+                        <h4>Rythym and routines</h4>
+
+                        <p>Description</p>
+                    </article>
+
+                    <article>
+                        <h4>Bending time</h4>
+
+                        <p>Description</p>
+                    </article>
+
+                    <article>
+                        <h4>Rythym and routines</h4>
+
+                        <p>Description</p>
+                    </article>
+
+                    <button>Show more ↑</button>
+                </div>
+            </section>
+
+            <section id="lets-play">
+                <header>
+                    <img src="...">
+                    <h2>Let's play</h2>
+
+                    <p><strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.</p>
+                </header>
+
+                <div class="side">
+                    <h3>Select →</h3>
+
+                    <p>We've been signaling patterns where we recongnize opportunities for building processes and turning practicies, that facilitate participation fulfillment, and involvement.</p>
+                </div>
+
+                <div>
+                    <article>
+                        <h4>Team as a service</h4>
+
+                        <p>Long-term project</p>
+                    </article>
+
+                    <article>
+                        <h4>Workshop series</h4>
+
+                        <p>Description</p>
+                    </article>
+
+                    <article>
+                        <h4>Design together</h4>
+
+                        <p>Description</p>
+                    </article>
+
+                    <button>Let's talk ↓</button>
+                </div>
+            </section>
         </main>
     </body>
 </html>
