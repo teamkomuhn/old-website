7c26976d6f125a5cd46e8e644adfaf9a66732571
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-04-12 17:36:58 +0100

    A lot of stuff

--- page-care.php
+++ page-care.php
@@ -3,10 +3,16 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width">
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@1,600&family=Inter:wght@400;700&display=swap" rel="stylesheet">
         
-        <link rel="stylesheet" href="<?php echo wp_make_link_relative(get_template_directory_uri() . '/styles.css') ?>">
+        <link rel="stylesheet" href="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/styles.css'; ?>" />
+
+        <script type="module" src="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/interaction.js'; ?>"></script>
 
         <title>Care</title>
     </head>
@@ -30,7 +36,7 @@
             </header>
 
             <aside>
-                <img src="...">
+                <img arin-hidden="true" src="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/read-the-post.svg'; ?>">
 
                 <p>Within many types of organizations — care — is often seen as something important, still, there's this uncertanty about its impact and how to put it into practice. How do we go about defining and manifesting it?</p>
 
@@ -38,124 +44,147 @@
             </aside>
 
             <section id="learnings">
-                <div class="side">
-                    <h2>Learnings →</h2>
+                <header class="side">
+                    <h2>Learnings</h2>
 
                     <p>We've been signaling patterns where we recongnize opportunities for building processes and turning practicies, that facilitate participation fulfillment, and involvement.</p>
-                </div>
+                </header>
 
-                <ul>
-                    <li><img src="..." />Identify opportunities for raising capacity</li>
-                    <li><img src="..." />Recognize power imbalences, understand its cause, and learn to propose distribution</li>
-                    <li><img src="..." />Enable practices that create awereness of positive habits and behaviors, and help detect obstacles to progress</li>
-                    <li><img src="..." />Facilitate activities that amplify strengths and consider everyone's needs</li>
-                    <li><img src="..." />Apply confilict as a device for supporting diversity</li>
-                    <li><img src="..." />Culture-in uncertain and complexity and tranform resistance into vulnerability and curiosity</li>
+                <ul class="cards">
+                    <li><img aria-hidden="true" src="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/lightning.svg'; ?>" />Identify opportunities for raising capacity</li>
+                    <li><img aria-hidden="true" src="..." />Recognize power imbalences, understand its cause, and learn to propose distribution</li>
+                    <li><img aria-hidden="true" src="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/eye.svg'; ?>" />Enable practices that create awereness of positive habits and behaviors, and help detect obstacles to progress</li>
+                    <li><img aria-hidden="true" src="..." />Facilitate activities that amplify strengths and consider everyone's needs</li>
+                    <li><img aria-hidden="true" src="..." />Apply confilict as a device for supporting diversity</li>
+                    <li><img aria-hidden="true" src="..." />Culture-in uncertain and complexity and tranform resistance into vulnerability and curiosity</li>
                 </ul>
             </section>
 
-            <section id="why-are-we-together">
-                <h2>Why are we together?</h2>
+            <section id="making-together">
+                <header>
+                    <div>
+                        <h2>Why are we together?</h2>
 
-                <p><strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.</p>
+                        <p><strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.</p>
+                    </div>
 
-                <div class="side">
-                    <h3>Themes and topics →</h3>
-            
-                    <p>We've been signaling patterns where we recongnize opportunities for building processes and turning practicies, that facilitate participation fulfillment, and involvement.</p>
-                </div>
+                    <div class="side">
+                        <h3>Themes and topics</h3>
+                
+                        <p>We've been signaling patterns where we recongnize opportunities for building processes and turning practicies, that facilitate participation fulfillment, and involvement.</p>
+                    </div>
+                </header>
 
                 <div>
-                    <article>
-                        <h4>Mapping needs</h4>
+                    <div class="cards">
+                        <article>
+                            <h4>Mapping needs</h4>
 
-                        <p>Who we are changes how we are. Make visible the needs of An exercise to make visible the needs</p>
+                            <p>Who we are changes how we are. Make visible the needs of An exercise to make visible the needs</p>
 
-                        <p>outline everybody's needs and facilitate conversations</p>
-                    </article>
+                            <p>outline everybody's needs and facilitate conversations</p>
+                        </article>
 
-                    <article>
-                        <h4>Beyond rules</h4>
+                        <article>
+                            <h4>Beyond rules</h4>
 
-                        <p>What if we could organize and be without punishment and</p>
-                    </article>
+                            <p>What if we could organize and be without punishment and</p>
+                        </article>
 
-                    <article>
-                        <h4>Bending time</h4>
+                        <article>
+                            <h4>Bending time</h4>
 
-                        <p>Description</p>
-                    </article>
+                            <p>Description</p>
+                        </article>
 
-                    <article>
-                        <h4>Rythym and routines</h4>
+                        <article>
+                            <h4>Rythym and routines</h4>
 
-                        <p>Description</p>
-                    </article>
+                            <p>Description</p>
+                        </article>
 
-                    <article>
-                        <h4>Bending time</h4>
+                        <article>
+                            <h4>Bending time</h4>
 
-                        <p>Description</p>
-                    </article>
+                            <p>Description</p>
+                        </article>
 
-                    <article>
-                        <h4>Rythym and routines</h4>
+                        <article>
+                            <h4>Rythym and routines</h4>
 
-                        <p>Description</p>
-                    </article>
+                            <p>Description</p>
+                        </article>
 
-                    <article>
-                        <h4>Bending time</h4>
+                        <article>
+                            <h4>Bending time</h4>
 
-                        <p>Description</p>
-                    </article>
+                            <p>Description</p>
+                        </article>
 
-                    <article>
-                        <h4>Rythym and routines</h4>
+                        <article>
+                            <h4>Rythym and routines</h4>
 
-                        <p>Description</p>
-                    </article>
+                            <p>Description</p>
+                        </article>
+                    </div>
 
-                    <button>Show more ↑</button>
+                    <button type="button">Show more ↑</button>
                 </div>
             </section>
 
             <section id="lets-play">
                 <header>
-                    <img src="...">
-                    <h2>Let's play</h2>
+                    <div>
+                        <img src="...">
+                        <h2>Let's play</h2>
 
-                    <p><strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.</p>
+                        <p><strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.</p>
+                    </div>
+
+                    <div class="side">
+                        <h3>Select</h3>
+
+                        <p>We've been signaling patterns where we recongnize opportunities for building processes and turning practicies, that facilitate participation fulfillment, and involvement.</p>
+                    </div>
                 </header>
 
-                <div class="side">
-                    <h3>Select →</h3>
-
-                    <p>We've been signaling patterns where we recongnize opportunities for building processes and turning practicies, that facilitate participation fulfillment, and involvement.</p>
-                </div>
-
                 <div>
-                    <article>
-                        <h4>Team as a service</h4>
+                    <div class="cards">
+                        <article>
+                            <h4>Team as a service</h4>
 
-                        <p>Long-term project</p>
-                    </article>
+                            <p>Long-term project</p>
+                        </article>
 
-                    <article>
-                        <h4>Workshop series</h4>
+                        <article>
+                            <h4>Workshop series</h4>
 
-                        <p>Description</p>
-                    </article>
+                            <p>Description</p>
+                        </article>
 
-                    <article>
-                        <h4>Design together</h4>
+                        <article>
+                            <h4>Design together</h4>
 
-                        <p>Description</p>
-                    </article>
+                            <p>Description</p>
+                        </article>
+                    </div>
 
-                    <button>Let's talk ↓</button>
+                    <button type="button">Let's talk ↓</button>
                 </div>
             </section>
+
+            <!-- <section id="masters-of-care">
+                <h2>Masters of care</h2>
+
+                <p><strong>Always searching for</strong> deeper understanding, fairness, and better ways of making together. Is this you?</p>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </section> -->
         </main>
     </body>
 </html>
