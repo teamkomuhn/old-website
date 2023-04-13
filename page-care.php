7d7eff048253aa39e5379d1c76a8bdc5c301cd2f
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-04-13 23:10:03 +0100

    Mobile

--- page-care.php
+++ page-care.php
@@ -1,44 +1,56 @@
+<?php 
+    declare(strict_types = 1);
+
+    function url(string $url) {
+        return wp_make_link_relative(get_template_directory_uri()) . $url;
+    }
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
+        <!-- <meta charset="UTF-8"> -->
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
         <meta name="viewport" content="width=device-width">
+
+        <title>Care</title>
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
         <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@1,600&family=Inter:wght@400;700&display=swap" rel="stylesheet">
         
-        <link rel="stylesheet" href="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/styles.css'; ?>" />
+        <link rel="stylesheet" href="<?= url('/styles.css'); ?>" />
 
-        <script type="module" src="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/interaction.js'; ?>"></script>
-
-        <title>Care</title>
+        <script type="module" src="<?= url('/interaction.js'); ?>"></script>
     </head>
     
     <body>
         <nav>
-            <a href="/">Komuhn</a>
+            <a id="ok" href="/">OK</a>
         </nav>
 
         <main>
             <header>
-                <h1>Fluency<br />in <span id="care">care</span></h1>
+                <div id="header-titles">
+                    <h1>Fluency<br />in <span id="care">care</span></h1>
 
-                <div>
-                    <h2>Thriving organazations through empathy</h2>
+                    <h2>Thriving organizations through empathy</h2>
+                </div>
 
-                    <p><strong>How we build capacity for care</strong> within our communities, organizations, teams has a critical effect on how much we can participate, accomplish, and commit to.</p>
+                <div id="header-content">
+                    <p><strong>How we build capacity for care</strong> within our communities, organizations, teams, has a critical effect on how much we can participate, accomplish, and invest in.</p>
 
-                    <p>In this proposal, we are exploring ways towards more sustainable collaborations by understanding the value of beeing together.</p>
+                    <p>In this proposal, we are exploring ways towards more sustainable collaborations by understanding the value of being together.</p>
                 </div>
             </header>
 
             <aside>
-                <img arin-hidden="true" src="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/read-the-post.svg'; ?>">
+                <img arin-hidden="true" src="<?= url('/read-the-post.svg'); ?>">
 
-                <p>Within many types of organizations — care — is often seen as something important, still, there's this uncertanty about its impact and how to put it into practice. How do we go about defining and manifesting it?</p>
+                <p>Within many types of organizations — care — is often seen as something important, still, there's this uncertainty about its impact or how to put it into practice. How do we go about defining and manifesting it?</p>
 
                 <a class="button" href="#">Read the post</a>
             </aside>
@@ -51,9 +63,9 @@
                 </header>
 
                 <ul class="cards">
-                    <li><img aria-hidden="true" src="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/lightning.svg'; ?>" />Identify opportunities for raising capacity</li>
+                    <li><img aria-hidden="true" src="<?= url('/lightning.svg'); ?>" />Identify opportunities for raising capacity</li>
                     <li><img aria-hidden="true" src="..." />Recognize power imbalences, understand its cause, and learn to propose distribution</li>
-                    <li><img aria-hidden="true" src="<?php echo wp_make_link_relative(get_template_directory_uri()) . '/eye.svg'; ?>" />Enable practices that create awereness of positive habits and behaviors, and help detect obstacles to progress</li>
+                    <li><img aria-hidden="true" src="<?= url('/eye.svg'); ?>" />Enable practices that create awereness of positive habits and behaviors, and help detect obstacles to progress</li>
                     <li><img aria-hidden="true" src="..." />Facilitate activities that amplify strengths and consider everyone's needs</li>
                     <li><img aria-hidden="true" src="..." />Apply confilict as a device for supporting diversity</li>
                     <li><img aria-hidden="true" src="..." />Culture-in uncertain and complexity and tranform resistance into vulnerability and curiosity</li>
@@ -128,7 +140,7 @@
                         </article>
                     </div>
 
-                    <button type="button">Show more ↑</button>
+                    <button type="button">Show more<span aria-hidden="true"> ↑</span></button>
                 </div>
             </section>
 
@@ -169,22 +181,22 @@
                         </article>
                     </div>
 
-                    <button type="button">Let's talk ↓</button>
+                    <button type="button">Let's talk<span aria-hidden="true">  ↓</span></button>
                 </div>
             </section>
 
-            <!-- <section id="masters-of-care">
+            <section id="masters-of-care">
                 <h2>Masters of care</h2>
 
                 <p><strong>Always searching for</strong> deeper understanding, fairness, and better ways of making together. Is this you?</p>
 
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li><span>You're part of a <strong>team</strong> that wants to go the long haul and is looking for ways to create <strong>sustainable involvement</strong>.</span></li>
+                    <li><span>You want to build a culture in your <strong>organization</strong> that enables people to be with uncertainty and <strong>tackle difficult issues</strong>.</span></li>
+                    <li /><img src="<?= url('/hands-3.svg'); ?>');" /><span>You lead a <strong>community</strong> project that is seeking more <strong>investment</strong> from the participants.</span></li>
+                    <li><span>You're always looking for self-improvement and want to learn more about <strong>fair and thriving collaborations</strong>.</span></li>
                 </ul>
-            </section> -->
+            </section>
         </main>
     </body>
 </html>
