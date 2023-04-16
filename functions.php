771e0f686b98b268eac816d236b86b54f211867a
aadonis <andrea@komuhn.co>
2023-04-16 19:00:17 +0100

    Add favicon

--- functions.php
+++ functions.php
@@ -1,4 +1,10 @@
 <?php
+
+global $post;
+$post_id = $post->ID;
+
+define('THEME_DIR', get_template_directory_uri());
+
 add_theme_support('post-thumbnails');
 
 
@@ -8,12 +14,12 @@ function openGraph(){
     $website_description = strip_tags(get_bloginfo( 'description' ));
     $homepage_excerpt = "Collaboration is not about gluing together existing egos. It's about the ideas that never existed until after everyone entered the room.";
     $type = is_single() ? 'article' : 'website';
-    $title = is_singular() ? get_the_title($post) : $website_name . " - " . $website_description;
-    $description = is_front_page() ? $homepage_excerpt : get_the_excerpt($post);
+    $title = is_singular() ? get_the_title($post_id) : $website_name . " - " . $website_description;
+    $description = is_front_page() ? $homepage_excerpt : get_the_excerpt($post_id);
 
     //replace this with a default image
-	$default_image = get_template_directory_uri() . "/img/open-graph-image.jpg"; 
-	$post_featured_image = get_post_thumbnail_id($post);
+	$default_image = get_template_directory_uri() . "/images/open-graph-image-default.jpg"; 
+	$post_featured_image = get_post_thumbnail_id($post_id);
 
 	if ( !empty($post_featured_image) ){
 		$image = wp_get_attachment_image_url($post_featured_image, 'medium');
--- header.php
+++ header.php
@@ -16,6 +16,11 @@
         <title><?= is_singular() ? get_the_title($post) . " - " . get_bloginfo( 'name' ) : get_bloginfo( 'name' ) . " - " . strip_tags(get_bloginfo( 'description' )); ?></title>
         <?php openGraph(); ?>
 
+        <link rel="icon" href="<?= get_template_directory_uri() . "/images/favicon.ico"; ?>" sizes="any"><!-- 32×32 -->
+        <link rel="icon" href="<?= get_template_directory_uri() . "/images/favicon.svg"; ?>" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="<?= get_template_directory_uri() . "/images/favicon-apple-touch-icon.png" ?>"><!-- 180×180 -->
+        <!--<link rel="manifest" href="/images/manifest.webmanifest";"-->
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
