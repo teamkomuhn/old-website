73eea815879643b30c13130110ec7e51c2f8e9c7
aadonis <andrea@komuhn.co>
2023-04-16 18:01:22 +0100

    Add twitter open graph tags + Adjustments to open graph

--- functions.php
+++ functions.php
@@ -6,25 +6,32 @@ function openGraph(){
 	
 	$website_name = get_bloginfo( 'name' );
     $website_description = strip_tags(get_bloginfo( 'description' ));
+    $homepage_excerpt = "Collaboration is not about gluing together existing egos. It's about the ideas that never existed until after everyone entered the room.";
     $type = is_single() ? 'article' : 'website';
     $title = is_singular() ? get_the_title($post) : $website_name . " - " . $website_description;
-    $description = is_front_page() ? $website_description : get_the_excerpt($post);
+    $description = is_front_page() ? $homepage_excerpt : get_the_excerpt($post);
 
     //replace this with a default image
 	$default_image = get_template_directory_uri() . "/img/open-graph-image.jpg"; 
 	$post_featured_image = get_post_thumbnail_id($post);
 
 	if ( !empty($post_featured_image) ){
-		$featured_image = wp_get_attachment_image_url($post_featured_image, 'medium');
+		$image = wp_get_attachment_image_url($post_featured_image, 'medium');
 	} else {
-		$featured_image = $default_image;
+		$image = $default_image;
 	}
     
-    echo '<meta property="og:url" content="' . get_permalink($post) . '"/>';
+    echo '<meta property="og:url" content="'. get_permalink($post) .'"/>';
     echo '<meta property="og:title" content="'. $title .'"/>';
-    echo '<meta property="og:description" content="' . $description . '"/>';
-    echo '<meta property="og:type" content="'.$type.'"/>';
-    echo '<meta property="og:image" content="' . $featured_image . '"/>';
+    echo '<meta property="og:description" content="'. $description .'"/>';
+    echo '<meta property="og:type" content="'. $type .'"/>';
+    echo '<meta property="og:image" content="'. $image .'"/>';
+
+    echo '<meta name="twitter:card" content="summary_large_image" />';
+    echo '<meta name="twitter:site" content="'. $website_name .'" />';
+    echo '<meta name="twitter:title" content="'. $title .'" />';
+    echo '<meta name="twitter:description" content="'. $description .'" />';
+    echo '<meta name="twitter:image" content="'. $image .'" />';
 }
 
 
