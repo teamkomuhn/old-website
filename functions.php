4630f3fe5d7cabc052b14b00a8e27ec5ae1ae890
aadonis <andrea@komuhn.co>
2023-04-16 20:19:21 +0100

    Fix functions variable error

--- functions.php
+++ functions.php
@@ -1,8 +1,5 @@
 <?php
 
-global $post;
-$post_id = $post->ID;
-
 define('THEME_DIR', get_template_directory_uri());
 
 add_theme_support('post-thumbnails');
@@ -22,12 +19,12 @@ function openGraph(){
     $website_description = strip_tags(get_bloginfo( 'description' ));
     $homepage_excerpt = "Collaboration is not about gluing together existing egos. It's about the ideas that never existed until after everyone entered the room.";
     $type = is_single() ? 'article' : 'website';
-    $title = is_singular() ? get_the_title($post_id) : $website_name . " - " . $website_description;
-    $description = is_front_page() ? $homepage_excerpt : get_the_excerpt($post_id);
+    $title = is_singular() ? get_the_title() : $website_name . " - " . $website_description;
+    $description = is_front_page() ? $homepage_excerpt : get_the_excerpt();
 
     //replace this with a default image
 	$default_image = get_template_directory_uri() . "/images/open-graph-image-default.jpg"; 
-	$post_featured_image = get_post_thumbnail_id($post_id);
+	$post_featured_image = get_post_thumbnail_id();
 
 	if ( !empty($post_featured_image) ){
 		$image = wp_get_attachment_image_url($post_featured_image, 'medium');
@@ -35,7 +32,7 @@ function openGraph(){
 		$image = $default_image;
 	}
     
-    echo '<meta property="og:url" content="'. get_permalink($post) .'"/>';
+    echo '<meta property="og:url" content="'. get_permalink() .'"/>';
     echo '<meta property="og:title" content="'. $title .'"/>';
     echo '<meta property="og:description" content="'. $description .'"/>';
     echo '<meta property="og:type" content="'. $type .'"/>';
