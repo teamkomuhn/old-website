41b47afc10e4905d7642ef0ce0ffc28f3b3307f7
aadonis <andrea@komuhn.co>
2023-04-17 19:28:39 +0100

    Add posts loop back

--- page-index.php
+++ page-index.php
@@ -7,8 +7,10 @@
     <p>Collaboration is not about gluing together existing egos. It's about the ideas that never existed until after everyone entered the room.</p>
 </header>
 
+
 <!-- TODO Update this section to be dynamic, as one for highlights, fetching a page/post/etc -->
 <section id="recent-work">
+    
     <h2><a class="arrow-down" href="#recent-work">Recent work</a></h2>
 
     <div>
@@ -31,9 +33,73 @@
             </div>
         </div>
     </div>
+
 </section>
 
+<?php //GET POSTS
+    $args = array(
+        'post_type'         => array('post'),
+        'category_name'     => 'post',
+        'post_status'       => array('publish'),
+        'posts_per_page'    => '10',
+        'order'             => 'DESC',
+        'orderby'           => 'date',
+    );
+
+    $ko_posts = new WP_Query( $args );
+    if ( $ko_posts -> have_posts() ):
+?>
+
+<section id="recent-posts">
+
+    <h2><a class="arrow-down" href="#recent-posts">Recent posts</a></h2>
+
+    <div>
+        <?php
+            while ( $ko_posts -> have_posts() ): $ko_posts -> the_post();
+        ?>
+
+        <article>
+            <?php
+                $post_image = get_the_post_thumbnail();
+                if (!empty($post_image)) { echo $post_image; }
+            ?>
+
+            <div>
+                <h3><?= get_the_title(); ?></h3>
+                <h4><?= get_post_meta(get_the_ID(), 'subtitle', true); ?></h4>
+
+
+                <div class="author short">
+                    <img aria-hidden="true" src="<?= get_avatar_url( get_the_author_meta( 'ID' ) ); ?>" />
+
+                    <div>
+                        <address>
+                            <?= get_the_author_meta('first_name') . ' '. get_the_author_meta('last_name'); ?>
+                        </address>
+
+                        <time datetime="<?= get_the_date('c'); ?>">
+                            <?= get_the_date('F j, Y'); ?>
+                        </time>
+                    </div>
+                </div>
+
+                <p><?= limit_characters(get_the_excerpt(), 150); ?></p>
+                <a class="arrow-right" href="<?= get_the_permalink(); ?>">Read more</a>
+            </div>              
+            </article>
+
+        <?php endwhile; ?>
+    </div>
+
+    <button class="arrow-up">See more</button>
+    
+</section>
+
+<?php endif; wp_reset_postdata(); ?>
+
 <section id="about">
+
     <header>
         <h2>Jacks of all trades.<br/>Masters of <strong>some</strong>.</h2>
 
@@ -81,9 +147,11 @@
             </div>
         </article>
     </div>
+
 </section>
 
 <section id="partners-clients-collaborators">
+
     <header>
         <h2>Dance like nobody is watching, <strong>collaborate</strong> like everyone is.</h2>
 
@@ -112,6 +180,7 @@
         <img src="<?= url('/images/logo-seaentia.svg'); ?>" alt="Seaentia">
         <img src="<?= url('/images/logo-colab.svg'); ?>" alt="COLAB+">
     </div>
+
 </section>
 
 <?php 
