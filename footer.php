227d1e6a682b35de27b2b8d1762f0270e84b6c12
aadonis <andrea@komuhn.co>
2023-04-17 11:08:17 +0100

    Remove GA script

--- footer.php
+++ footer.php
@@ -15,15 +15,4 @@
         </footer>
         
         <?php //wp_footer(); ?>
-
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JJN9Q36MB0"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-JJN9Q36MB0');
-        </script>
-    </body>
 </html>
