5277308a323512a025cbd7a7d54e1ac0b0eb6056
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2024-05-29 18:02:50 +0100

    Make full logo in footer clickable

--- footer.php
+++ footer.php
@@ -3,8 +3,10 @@
 		<?php comments_template('/contact.php'); ?>
 
 		<footer id="footer">
-			<img aria-hidden="true" src="<?= get_theme_file_uri(is_404() ? '/images/komuhn-light.svg' : '/images/komuhn.svg') ?>" />
-			<a href='<?= home_url() ?>'>komuhn.org</a>
+			<a href="<?= home_url() ?>">
+				<img aria-hidden="true" src="<?= get_theme_file_uri(is_404() ? '/images/komuhn-light.svg' : '/images/komuhn.svg') ?>" />
+				komuhn.org
+			</a>
 		</footer>
 
 		<?php wp_footer(); ?>
